a881308258d1227671f01f57422149bfde923576
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-01 17:42:40 +0700

    tambah role user sebagai default akun yang daftar

--- register.php
+++ register.php
@@ -5,12 +5,7 @@ $password = $_POST['Password'];
 $Email = $_POST['Email'];
 $Alamat = $_POST['Alamat'];
 $NamaLengkap = $_POST['NamaLengkap'];
-$level = 1;
-
-$data = mysqli_query($koneksi, "INSERT INTO user VALUES ('', '$username', '$password', '$Email', '$NamaLengkap', '$Alamat')");
-if ($level == 1) {
-    header("location: index.html");
-} else {
-    header("location: admin/index.php");
-}
+$role = 'user';
+$data = mysqli_query($koneksi, "INSERT INTO user VALUES ('', '$username', '$password', '$Email', '$NamaLengkap', '$Alamat', '$role')");
+header("location: index.html");
 ?>
