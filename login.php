fe66e98163593cd8eff2bb6bca11f04f300b1cb1
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-01 17:38:07 +0700

    tambah login akun berdasarkan role

--- login.php
+++ login.php
@@ -3,15 +3,17 @@ session_start();
 include 'koneksi.php';
 $username = $_GET['Username'];
 $password = $_GET['Password'];
-
 $data = mysqli_query($koneksi,"select * from user where Username ='$username'and Password='$password'");
 $cek = mysqli_num_rows($data);
-
-if($cek > 0){
+$role = mysqli_fetch_array($data)['role'];
+if($cek > 0 && $role == 'user'){
     $_SESSION['username']= $username;
     $_SESSION['status'] = "login";
     header ("location: index.html");
+}else if($cek > 0 && $role == 'admin' or $role == 'petugas'){
+    header ("location: admin/index.html");
 }else{
-    header ("location: index.php?pesan=gagal");
+    header ("location: login.html?pesan=gagal");
 }
+
 ?>
