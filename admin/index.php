611b8a105d4884a15f9c4228a0b6d1e1d6be8099
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-02 23:02:07 +0700

    add card statistic

--- admin/index.php
+++ admin/index.php
@@ -5,21 +5,59 @@ if (!isset($_SESSION['status']) || $_SESSION['role'] !== 'admin') {
     exit;
 } else {
     include '../layout/sidebar-navbar-footbar.php';
+    include '../koneksi.php';
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dashboard Admin</title>
-</head>
+<style>
+    @media (min-width: 992px) {
+  body {
+    margin-left: 240px; /* Lebar sidebar */
+  }
+}
+
+</style>
 <body>
-    <div class="container-fluid justify-content-center col-4">
-        <h1 class="mt-4">Dashboard Admin</h1>
-        <p>Selamat datang di dashboard admin perpustakaan digital.</p>
-        <p>Anda dapat mengelola data buku, anggota, dan transaksi peminjaman di sini.</p>
-        <p>Silakan pilih menu di sidebar untuk mulai mengelola data.</p>
+<h1 class="mt-4">Dashboard</h1>
+    <div class="row mx-5">
+        <div class="col-4">
+            <div class="card bg-primary text-white mb-4">
+                <div class="card-body">
+                <?php
+                    echo mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM buku"));
+                ?>
+                Total Buku</div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretche-link" href="#"> View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card bg-success text-white mb-4">
+                <div class="card-body">
+                <?php
+                    echo mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM user"));
+                ?>    
+                Total User</div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretche-link" href="#"> View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card bg-warning text-white mb-4">
+                <div class="card-body">
+                <?php
+                    echo mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM peminjaman"));
+                ?>    
+                Total Peminjaman</div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretche-link" href="#"> View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
