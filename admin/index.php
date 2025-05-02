b6e522164567652714cd27f128e98398c5336c92
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-02 21:03:05 +0700

    modified index.php

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,19 @@
+<?php
+include '../layout/sidebar-navbar-footbar.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>dashboard Admin</title>
 </head>
 <body>
-    
+    <div class="container-fluid justify-content-center col-4">
+        <h1 class="mt-4">Dashboard Admin</h1>
+        <p>Selamat datang di dashboard admin perpustakaan digital.</p>
+        <p>Anda dapat mengelola data buku, anggota, dan transaksi peminjaman di sini.</p>
+        <p>Silakan pilih menu di sidebar untuk mulai mengelola data.</p>
+    </div>
 </body>
 </html>
