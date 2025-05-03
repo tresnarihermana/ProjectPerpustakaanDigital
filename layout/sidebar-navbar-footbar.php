6b95ca06ce8fd6f953e7cf91d0a0564cf15412c8
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-03 16:59:19 +0700

    update responsive hapus mdb jadi bs

--- layout/sidebar-navbar-footbar.php
+++ layout/sidebar-navbar-footbar.php
@@ -96,73 +96,40 @@ body {
   <!-- Sidebar -->
 
   <!-- Navbar -->
-  <nav id="main-navbar" class="navbar navbar-expand-lg bg-dark fixed-top">
-    <!-- Container wrapper -->
-    <div class="container-fluid">
-      <!-- Toggle button -->
-      <button data-mdb-button-init
-        class="navbar-toggler"
-        type="button"
-        data-mdb-collapse-init
-        data-mdb-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
+<nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+    <!-- Toggle button -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-      <!-- Brand -->
-      <a class="navbar-brand" href="#">
-        <img
-          src="../storage/img/logo.svg"
-          height="50px"
-          alt="PD Logo"
-          loading="lazy"
-        />
-        <span style="color: white;">Perpustakaan Digital</span>
-      </a>
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">
+      <img src="../storage/img/logo.svg" height="50px" alt="PD Logo" loading="lazy" />
+      <span style="color: white;">Perpustakaan Digital</span>
+    </a>
 
-      <!-- Right links -->
+    <!-- Right links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto d-flex flex-row">
-    
-    
-    
         <!-- Avatar -->
         <li class="nav-item dropdown">
-          <a
-            data-mdb-dropdown-init class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center"
-            href="#"
-            id="navbarDropdownMenuLink"
-            role="button"
-            data-mdb-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="../storage/img/user.png"
-              class="rounded-circle"
-              height="22"
-              alt="Avatar"
-              loading="lazy"
-            />
+          <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="../storage/img/user.png" class="rounded-circle" height="22" alt="Avatar" loading="lazy" />
           </a>
-          <ul
-            class="dropdown-menu dropdown-menu-end"
-            aria-labelledby="navbarDropdownMenuLink"
-          >
-            <li>
-              <a class="dropdown-item" href="../index.php">Halaman Utama</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="../logout.php">Logout</a>
-            </li>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="../index.php">Halaman Utama</a></li>
+            <li><a class="dropdown-item" href="../logout.php">Logout</a></li>
           </ul>
         </li>
       </ul>
     </div>
-    <!-- Container wrapper -->
-  </nav>
-  <!-- Navbar -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
+
 </header>
 <!--Main Navigation-->
 
@@ -178,7 +145,7 @@ body {
 </footer>
     
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 </body>
 </html>
