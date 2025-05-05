0f3b6b1e41d43e30cc02f2023fb1732a9f394d24
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-05 18:26:14 +0700

    add alert.php

---
+++ layout/alert.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_GET['pesan'])) {
+    // Cek pesan di URL dan tampilkan pesan yang sesuai
+    if ($_GET['pesan'] == 'berhasil') {
+        echo '<div class="alert alert-success">Operasi berhasil dilakukan!</div>';
+    } elseif ($_GET['pesan'] == 'gagal') {
+        echo '<div class="alert alert-danger">Terjadi kesalahan, coba lagi.</div>';
+    } elseif ($_GET['pesan'] == 'duplikat') {
+        echo '<div class="alert alert-warning">Nama kategori sudah digunakan!</div>';
+    }
+}
+?>
