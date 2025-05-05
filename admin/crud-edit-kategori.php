bfb0844512804addcecd6c2951677227961f1d42
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-05 18:25:38 +0700

    fix bug duplikat

--- admin/crud-edit-kategori.php
+++ admin/crud-edit-kategori.php
@@ -2,9 +2,9 @@
 include '../koneksi.php';
 $namakategori = $_POST['namakategori'];
 $kategoriID = $_POST['kategoriID'];
-$data = mysqli_query($koneksi, "SELECT * FROM kategoribuku WHERE Namakategori = '$namakategori' AND KategoriID = '$kategoriID'");
+$data = mysqli_query($koneksi, "SELECT * FROM kategoribuku WHERE Namakategori = '$namakategori' AND KategoriID != '$kategoriID'");
 $cek = mysqli_fetch_assoc($data);
- if (mysqli_num_rows($data) == 0) {
+ if (mysqli_num_rows($data) > 0) {
     header("location: kategori.php?pesan=duplikat");
 } else {
     $query = "UPDATE kategoribuku SET Namakategori = '$namakategori' WHERE KategoriID = '$kategoriID'";
