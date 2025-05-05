ad50ae1d0703bc904f73581e6ec45ad6437cd251
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-05 22:38:22 +0700

    add fontawesome icon to each alert

--- layout/alert.php
+++ layout/alert.php
@@ -2,11 +2,11 @@
 if (isset($_GET['pesan'])) {
     // Cek pesan di URL dan tampilkan pesan yang sesuai
     if ($_GET['pesan'] == 'berhasil') {
-        echo '<div class="alert alert-success">Operasi berhasil dilakukan!</div>';
+        echo '<div class="alert alert-success"><i class="fa-solid fa-check"></i> Operasi berhasil dilakukan!</div>';
     } elseif ($_GET['pesan'] == 'gagal') {
-        echo '<div class="alert alert-danger">Terjadi kesalahan, coba lagi.</div>';
+        echo '<div class="alert alert-danger"><i class="fa-solid fa-triangle-exclamation" style="color: #ff0f0f;"></i> Terjadi kesalahan, coba lagi.</div>';
     } elseif ($_GET['pesan'] == 'duplikat') {
-        echo '<div class="alert alert-warning">Duplikasi data Terdeteksi! coba lagi</div>';
+        echo '<div class="alert alert-warning"><i class="fa-solid fa-clone"></i> Duplikasi data Terdeteksi! coba lagi</div>';
     }
 }
 ?>
