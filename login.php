ac65fc76c9d8ccce5271f57c6423d3a2f1ec2e94
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-05 22:39:26 +0700

    fixed login error

--- login.php
+++ login.php
@@ -7,14 +7,14 @@ $password = $_POST['Password'];
 
 $data = mysqli_query($koneksi,"SELECT * FROM user WHERE Username='$username' AND Password='$password'");
 $cek = mysqli_num_rows($data);
+$user = mysqli_fetch_assoc($data);
+$role = $user['role'];
+
+$_SESSION['username'] = $username;
+$_SESSION['status'] = 'login';
+$_SESSION['role'] = $role;
 
 if($cek > 0){
-    $user = mysqli_fetch_assoc($data);
-    $role = $user['role'];
-
-    $_SESSION['username'] = $username;
-    $_SESSION['status'] = 'login';
-    $_SESSION['role'] = $role;
 
     if($role == 'user'){
         header("location: index.php");
