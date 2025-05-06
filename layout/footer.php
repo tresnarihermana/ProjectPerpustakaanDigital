ca6bedc54a653fb6082b28f903871e2bd294b9a2
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-06 21:29:05 +0700

    add footer.php

---
+++ layout/footer.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Footer</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+  <style>
+    footer {
+      background-color: #1e1e1e;
+      color: white;
+      margin-top: 100px;
+    }
+    .footer-logo {
+      height: 60px;
+    }
+    .footer-icons i {
+      font-size: 1.5rem;
+      margin-right: 10px;
+      color: white;
+    }
+    .footer-section h6 {
+      font-weight: bold;
+      margin-bottom: 1rem;
+    }
+    .footer-section ul {
+      list-style: none;
+      padding: 0;
+      color: #9ea1a1;
+    }
+    .footer-section ul li {
+      margin-bottom: 0.5rem;;
+    }
+    .footer-section ul li:hover {
+      color: #8eaeff;
+      cursor: pointer;
+    }
+    .libre-baskerville-regular {
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: normal;
+    }
+
+    .libre-baskerville-bold {
+    font-family: "Libre Baskerville", serif;
+    font-weight: 700;
+    font-style: normal;
+    }
+
+    .libre-baskerville-regular-italic {
+    font-family: "Libre Baskerville", serif;
+    font-weight: 400;
+    font-style: italic;
+    }
+
+  </style>
+</head>
+<body>
+  <footer class="py-5">
+    <div class="container">
+      <div class="row">
+        <!-- Logo dan Sosial Media -->
+        <div class="col-md-3 mb-4">
+            <div class="d-flex align-items-center mb-3" onclick="window.location.href='#'" style="cursor: pointer;">
+              <img src="storage/img/logo.svg" alt="Perpustakaan Digital" class="footer-logo me-2" style="height: 64px;">
+              <div class="libre-baskerville-regular">
+                <h5 class="mb-0">Perpustakaan</h5>
+                <h5 class="mb-0">Digital</h5>
+              </div>
+            </div>
+          <div class="footer-icons mt-3">
+            <i class="bi bi-twitter-x"></i>
+            <i class="bi bi-instagram"></i>
+            <i class="bi bi-youtube"></i>
+            <i class="bi bi-linkedin"></i>
+          </div>
+        </div>
+        <!-- Use Cases Columns -->
+        <div class="col-md-3 footer-section">
+          <h6>Use cases</h6>
+          <ul>
+            <li><a href="#" class="nav-link">UI design</a></li>
+            <li><a href="#" class="nav-link">UX design</a></li>
+            <li><a href="#" class="nav-link">Wireframing</a></li>
+            <li><a href="#" class="nav-link">Diagramming</a></li>
+            <li><a href="#" class="nav-link">Brainstorming</a></li>
+            <li><a href="#" class="nav-link">Online whiteboard</a></li>
+            <li><a href="#" class="nav-link">Team collaboration</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 footer-section">
+          <h6>Use cases</h6>
+          <ul>
+            <li><a href="#" class="nav-link">UI design</a></li>
+            <li><a href="#" class="nav-link">UX design</a></li>
+            <li><a href="#" class="nav-link">Wireframing</a></li>
+            <li><a href="#" class="nav-link">Diagramming</a></li>
+            <li><a href="#" class="nav-link">Brainstorming</a></li>
+            <li><a href="#" class="nav-link">Online whiteboard</a></li>
+            <li><a href="#" class="nav-link">Team collaboration</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 footer-section">
+          <h6>Use cases</h6>
+          <ul>
+            <li><a href="#" class="nav-link">UI design</a></li>
+            <li><a href="#" class="nav-link">UX design</a></li>
+            <li><a href="#" class="nav-link">Wireframing</a></li>
+            <li><a href="#" class="nav-link">Diagramming</a></li>
+            <li><a href="#" class="nav-link">Brainstorming</a></li>
+            <li><a href="#" class="nav-link">Online whiteboard</a></li>
+            <li><a href="#" class="nav-link">Team collaboration</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>     -->
+</body>
+</html>
