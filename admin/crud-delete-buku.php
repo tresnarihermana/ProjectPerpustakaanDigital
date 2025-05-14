62e5a2a3c64e103d95e48e510cdfe16165c9fd03
ikanemo <fahmirizkii771@gmail.com>
2025-05-14 15:12:20 +0700

    add buku-edit, crud edit & delete buku

--- admin/crud-delete-buku.php
+++ admin/crud-delete-buku.php
@@ -6,15 +6,14 @@ $data = mysqli_query($koneksi, "SELECT * FROM buku WHERE BukuID = '$id'");
 $cek = mysqli_fetch_assoc($data);
 
 if (mysqli_num_rows($data) == 0) {
-    header("location: pengguna.php?pesan=gagal");
+    header("location: buku.php?pesan=gagali");
 } else {
-$query1 = mysqli_query($koneksi, "DELETE FROM ulasanbuku WHERE BukuID = '$id'");
-$query2 = mysqli_query($koneksi, "DELETE FROM buku WHERE BukuID = '$id'");
+$query = "DELETE FROM buku WHERE BukuID = '$id'";
 
-    if (mysqli_query($koneksi, $query1, $query2) && $id > 0) {
+    if (mysqli_query($koneksi, $query) && $id > 0) {
         header("location: buku.php?pesan=berhasil");
     } else {
-        header("location: buku.php?pesan=gagal");
+        header("location: buku.php?pesan=gagalo");
     }
 }
 
