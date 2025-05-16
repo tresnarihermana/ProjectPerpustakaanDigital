3420224ec5eee13892d1ba5b9fd3fbf85315d813
nadyanafisah <nanatfisah@gmail.com>
2025-05-16 16:03:47 +0700

    menambahkan ulasan buku

---
+++ crud-tambah-ulasan.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+
+$user = $_SESSION['UserID'];
+$buku = $_POST['buku_id'];
+$rating = $_POST['rating'];
+$ulasan = $_POST['ulasan'];
+
+$cek = mysqli_query($koneksi, "
+SELECT * FROM ulasanbuku 
+WHERE UserID = '$user' AND BukuID = '$buku'");
+if (mysqli_num_rows($cek) > 0){
+    header("location: ulasan.php?pesan=duplikat");
+}else{
+// Simpan ke database
+mysqli_query($koneksi, "
+  INSERT INTO ulasanbuku (UserID, BukuID, Ulasan, Rating) 
+        VALUES ('$user', '$buku', '$ulasan', '$rating')
+");
+
+header("Location: detail-buku.php?id=$buku");
+exit;
+}
+?>
