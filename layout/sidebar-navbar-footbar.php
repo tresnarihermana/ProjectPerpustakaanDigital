aaf8ca3180d479bd3f5f5bbcbae9a09e80713f3a
ikanemo <fahmirizkii771@gmail.com>
2025-05-21 10:54:43 +0700

    modified

--- layout/sidebar-navbar-footbar.php
+++ layout/sidebar-navbar-footbar.php
@@ -151,7 +151,7 @@ body {
 <!--Main layout-->
 <footer class="bg-body-tertiary text-center text-lg-start fixed-bottom">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2023 Copyright:
+      © 2025 Copyright:
       <a class="text-dark" href="#">Perpustakaan Digital</a>
 </footer>
     
