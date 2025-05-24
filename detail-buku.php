fc64a197390e0eb665d61819cad1592fb6b700ee
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-24 19:57:04 +0700

    fix modal ebook tidak menampilkan judul buku

--- detail-buku.php
+++ detail-buku.php
@@ -262,7 +262,7 @@ include 'layout/navbar.php';
     <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="pdfModalLabel">Baca eBook: Judul Buku</h5>
+          <h5 class="modal-title" id="pdfModalLabel">Baca eBook: <?= htmlspecialchars($buku['Judul'])?></h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
         </div>
         <div class="modal-body">
