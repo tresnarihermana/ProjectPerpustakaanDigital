ed40b72f6f0e0e763e21e7530869201203e97e09
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-24 16:35:22 +0700

    tambah fitur cek stok dan auto -1 stok bila peminjaman berhasil

--- user/proses-peminjaman.php
+++ user/proses-peminjaman.php
@@ -8,11 +8,20 @@ $tgl_pinjam = $_POST['tanggal_pinjam'];
 $tgl_kembali = $_POST['tanggal_kembali'];
 $status = "Dipinjam";
 
+// cek stok buku
+$cek_stok = mysqli_query($koneksi, "SELECT Stok FROM buku WHERE BukuID = '$buku'");
+$data_stok = mysqli_fetch_assoc($cek_stok);
+if ($data_stok['Stok'] <= 0) {
+    header('Location: ../daftar-peminjaman.php?pesan=stokhabis');
+    exit;
+}
+
+
 // Cek apakah user sudah meminjam buku ini dan belum dikembalikan
 $cek_duplikat = mysqli_query($koneksi, "SELECT * FROM peminjaman 
     WHERE BukuID = '$buku' AND UserID = '$user_id' AND StatusPeminjaman = '$status'");
 if (mysqli_num_rows($cek_duplikat) > 0) {
-    header('Location: ../daftar-peminjaman.php?pesan=duplikat');
+    header('Location: ../daftar-peminjaman.php?pesan=sudahdipinjam');
     exit;
 }
 
@@ -39,8 +48,8 @@ if ($selisih_hari > 7) {
 // Jika lolos semua validasi, lakukan peminjaman
 $query = "INSERT INTO peminjaman (UserID, BukuID, TanggalPeminjaman, TanggalPengembalian, StatusPeminjaman) 
           VALUES ('$user_id', '$buku', '$tgl_pinjam', '$tgl_kembali', '$status')";
-
-if (mysqli_query($koneksi, $query)) {
+$stok = mysqli_query($koneksi, "UPDATE buku SET Stok = Stok - 1 WHERE BukuID = '$buku'");
+if (mysqli_query($koneksi, $query) && $stok) {
     header('Location: ../daftar-peminjaman.php?pesan=berhasil');
 } else {
     header('Location: ../daftar-peminjaman.php?pesan=gagal');
