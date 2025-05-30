67058579224abed4c9b8dacb5eb9424765b04490
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-30 21:45:18 +0700

    add md5 and prepared statement untuk antisipasi sql injection

--- login.php
+++ login.php
@@ -3,27 +3,30 @@ session_start();
 include 'koneksi.php';
 
 $username = $_POST['Username'];
-$password = $_POST['Password'];
+$password = md5($_POST['Password']);
 
-$data = mysqli_query($koneksi,"SELECT * FROM user WHERE Username='$username' AND Password='$password'");
-$cek = mysqli_num_rows($data);
-$user = mysqli_fetch_assoc($data);
-$role = $user['role'];
-$userid = $user['UserID'];
+$stmt = $koneksi->prepare("SELECT UserID, role FROM user WHERE Username = ? AND Password = ?");
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+$result = $stmt->get_result();
 
-$_SESSION['username'] = $username;
-$_SESSION['status'] = 'login';
-$_SESSION['role'] = $role;
-$_SESSION['UserID'] = $userid;
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
 
-if($cek > 0){
+    $_SESSION['username'] = $username;
+    $_SESSION['status'] = 'login';
+    $_SESSION['role'] = $user['role'];
+    $_SESSION['UserID'] = $user['UserID'];
 
-    if($role == 'user'){
-        header("location: index.php");
-    } else if($role == 'admin' || $role == 'petugas'){
-        header("location: admin/index.php");
+    if ($user['role'] == 'user') {
+        header("Location: index.php");
+        exit();
+    } else if ($user['role'] == 'admin' || $user['role'] == 'petugas') {
+        header("Location: admin/index.php");
+        exit();
     }
 } else {
-    header("location: login.html?pesan=gagal");
+    header("Location: login.html?pesan=gagal");
+    exit();
 }
 ?>
