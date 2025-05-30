00dfb9e52e864947f6788baa0ab9ce58544b0608
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-30 21:52:08 +0700

    tambah pesan gagal, maxpinjam dan lain lain

--- admin/crud-add-peminjaman.php
+++ admin/crud-add-peminjaman.php
@@ -13,17 +13,27 @@ $buku = $_POST['buku'];
 $tgl_pinjam = $_POST['tgl_pinjam'];
 $tgl_kembali = $_POST['tgl_kembali'];
 $status = $_POST['status'];
-
-// // Validasi data (opsional, tapi disarankan)
-// if (empty($user) || empty($buku) || empty($tgl_pinjam) || empty($tgl_kembali) || empty($status)) {
-//     die("Semua data harus diisi!");
-// }
-
-// Masukkan ke database
+$cek_duplikat = mysqli_query($koneksi, "SELECT * FROM peminjaman WHERE UserID = '$user' AND BukuID = '$buku' AND StatusPeminjaman = 'dipinjam'");
+if (mysqli_num_rows($cek_duplikat) > 0) {
+    // Jika data sudah ada, redirect dengan pesan gagal
+    header('Location: peminjaman.php?pesan=duplikat');
+    exit;
+}
+$cek_batas = mysqli_query($koneksi, "SELECT COUNT(*) as total FROM peminjaman WHERE UserID = '$user' AND StatusPeminjaman = 'dipinjam'");
+$data_batas = mysqli_fetch_assoc($cek_batas);
+if ($data_batas['total'] >= 3) {
+    // Jika sudah meminjam 3 buku, redirect dengan pesan max pinjam,
+    header('Location: peminjaman.php?pesan=maxpinjam');
+    exit;
+}
 $query = "INSERT INTO peminjaman (UserID, BukuID, TanggalPeminjaman, TanggalPengembalian, StatusPeminjaman) 
           VALUES ('$user', '$buku', '$tgl_pinjam', '$tgl_kembali', '$status')";
-mysqli_query($koneksi, $query) or die("Gagal tambah data: " . mysqli_error($koneksi));
+// mysqli_query($koneksi, $query) or die("Gagal tambah data: " . mysqli_error($koneksi));
+if (!mysqli_query($koneksi, $query)) {
+    header('Location: peminjaman.php?pesan=gagal');
+} else {
+    header('Location: peminjaman.php?pesan=berhasil');
+}
 
-header('Location: peminjaman.php');
 exit;
 ?>
