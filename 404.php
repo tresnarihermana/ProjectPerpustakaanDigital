a7d139670ec355277dedafb82a72af1d28881298
Tresnarihermana <tresnarihermana@gmail.com>
2025-05-31 13:01:59 +0700

    tambah laman 404 page not found

---
+++ 404.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>404 - Halaman Tidak Ditemukan</title>
+  <link rel="icon" href="storage/img/logo.svg" type="image/x-icon">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
+  <style>
+  </style>
+</head>
+<body>
+<section class="d-flex align-items-center min-vh-100 py-5">
+        <div class="container py-5">
+            <div class="row align-items-center">
+                <div class="col-md-6 order-md-2">
+                    <div class="lc-block">
+                        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                        <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_kcsr6fcp.json" background="transparent" speed="1" loop="" autoplay=""></lottie-player>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center text-md-start ">
+                    <div class="lc-block mb-3">
+                        <div editable="rich">
+                            <!-- <h1 class="fw-bold h4">PAGE NOT FOUND!<br></h1> -->
+                        </div>
+                    </div>
+                    <div class="lc-block mb-3">
+                        <div editable="rich">
+                            <h1 class="display-1 fw-bold text-muted">Error 404</h1>
+
+                        </div>
+                    </div>
+                    <div class="lc-block mb-5">
+                        <div editable="rich">
+                            <p class="rfs-11 fw-light"> The page you are looking for was moved, removed or might never existed.</p>
+                        </div>
+                    </div>
+                    <div class="lc-block">
+                        <a class="btn btn-lg btn-secondary" href="index.php" role="button">Back to homepage</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
