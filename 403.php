0cc91a9eb4d40b3d817b35abedf7c1b60ac341ba
Tresnarihermana <tresnarihermana@gmail.com>
2025-06-02 19:50:33 +0700

    tambah laman 403 forbidden access bila user mencoba masuk ke dashboard admin

---
+++ 403.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>403 - Forbidden access</title>
+  <link rel="icon" href="storage/img/logo.svg" type="image/x-icon">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
+  <style>
+  </style>
+</head>
+<body>
+<section class="d-flex align-items-center min-vh-100">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 order-md-2">
+                    <div class="lc-block">
+                    <creattie-embed
+                    src="https://d1jj76g3lut4fe.cloudfront.net/saved_colors/122657/XLrhDUmWewxm4Eft.json"
+                    delay="1"
+                    speed="100"
+                    frame_rate="24"
+                    trigger="loop"
+                    style="width:600px;">
+                    </creattie-embed>
+                    <script src="https://creattie.com/js/embed.js?id=3f6954fde297cd31b441" defer></script>
+ 
+                </div>
+                </div>
+                <div class="col-md-6 text-center text-md-start ">
+                    <div class="lc-block mb-3">
+                        <div editable="rich">
+                            <!-- <h1 class="fw-bold h4">PAGE NOT FOUND!<br></h1> -->
+                        </div>
+                    </div>
+                    <div class="lc-block mb-3">
+                        <div editable="rich">
+                            <h1 class="display-1 fw-bold text-danger">Error 403</h1>
+                            <h2 class="display-6 fw-bold text-danger">Forbidden Access</h2>
+
+                        </div>
+                    </div>
+                    <div class="lc-block mb-5">
+                        <div editable="rich">
+                            <p class="rfs-11 fw-light"> You have no access to page you currently searching for. If you think there is some mistake, please contact the admin.</p>
+                        </div>
+                    </div>
+                    <div class="lc-block">
+                        <a class="btn btn-lg btn-warning" href="index.php" role="button">Back to homepage</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
